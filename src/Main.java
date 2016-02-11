6236300565ede24c1050c7479a8aca87f6e1b734
dance2die <dontbothermewithspam@gmail.com>
2016-02-11 15:03:52 -0500

    static import statement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
         for (String arg : args) {
-            System.out.println(arg);
+            out.println(arg);
         }
     }
 }
