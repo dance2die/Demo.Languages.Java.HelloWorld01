694b4a10f5eb58c8b99844210a701a194e1cca1f
dance2die <dontbothermewithspam@gmail.com>
2016-02-12 21:26:43 -0500

    tested variable argument

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,27 @@ public class Main {
 //        for (String arg : args) {
 //            out.println(arg);
 //        }
+//        testSavingsAccount();
 
+        testVargs();
+    }
+
+    private static void testVargs() {
+        System.out.println(sum(10,20,30));
+        System.out.println(sum(10,30,40));
+        System.out.println(sum(10,200,300));
+    }
+
+    private static double sum(double... values) {
+        double sum = 0;
+        for (double value : values) {
+            sum += value;
+        }
+
+        return sum;
+    }
+
+    private static void testSavingsAccount() {
         SavingsAccount savingsAccount = new SavingsAccount(100);
         savingsAccount.printBalance();
         savingsAccount.deposit(200).printBalance();
