264c96ad9b93705c1691dda2dce9e87c84f24798
dance2die <dontbothermewithspam@gmail.com>
2016-02-16 16:58:59 -0500

    UPDATED annotation

--- src/annotations/Stub.java
+++ src/annotations/Stub.java
@@ -1,12 +1,15 @@
 package annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Created by dance2die on 2/16/2016.
  */
 @Target(value = {ElementType.CONSTRUCTOR, ElementType.METHOD})
+@Retention(value= RetentionPolicy.SOURCE)
 public @interface Stub {
     int id();
     String dueDate();
