49e9c1ea141e8a9deb3072e14395ccc733018f13
dance2die <dontbothermewithspam@gmail.com>
2016-02-16 17:02:49 -0500

    Update annotation

--- src/Extend/Employee.java
+++ src/Extend/Employee.java
@@ -5,6 +5,7 @@ import annotations.Stub;
 /**
  * Created by dance2die on 2/13/2016.
  */
+@Stub(id = 1, dueDate = "11/25/2016")
 public class Employee extends Person {
     @Deprecated
     private int id;
@@ -19,6 +20,7 @@ public class Employee extends Person {
         return id;
     }
 
+    @Stub(id = 3, dueDate = "12/25/2016")
     public void setId(int id) {
         this.id = id;
     }
